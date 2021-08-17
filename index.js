c7e3722a20190e929d8da3215909c43d66233244
kazion500 <patrickckcabwe@gmail.com>
2021-08-17 19:03:21 +0200

    add webhook

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin:["*"]
+}));
 
 app.get("/", (req, res) => {
   res.send("working...");
